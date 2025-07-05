87d3aa13a0a3875af7365facd6007116fb16ac55
emamjion <emamjion@gmail.com>
2025-07-05 22:29:22 +0600

    added get all banners controller

---
+++ controllers/banner.controller.js
@@ -0,0 +1,15 @@
+import BannerModel from "../models/banner.model.js";
+
+const getAllBanners = async (req, res) => {
+  try {
+    const banners = await BannerModel.find();
+    res.json({
+      success: true,
+      message: "Banners fetched successfully!",
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch banners" });
+  }
+};
+
+export { getAllBanners };
--- models/banner.model.js
+++ models/banner.model.js
@@ -8,6 +8,7 @@ const bannerSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const Banner = mongoose.models.Banner || mongoose.model("Banner", bannerSchema);
+const BannerModel =
+  mongoose.models.Banner || mongoose.model("Banner", bannerSchema);
 
-export default Banner;
+export default BannerModel;
